eb127ed48a53c98b97362209b1720a9a1661286d
Blacktouro <salvador0988@sapo.pt>
2026-03-18 21:34:43 +0000

    mudar alguns ficheiros para mobile

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,16 +1,30 @@
+import { useState } from "react"
 import "../styles/header.css"
 
-export default function Header() {
+function Header() {
+
+  const [open, setOpen] = useState(false)
+
   return (
     <header className="header">
+
       <div className="logo">Vivace</div>
 
-      <nav className="nav">
-        <a href="#inicio">Início</a>
-        <a href="#servicos">Serviços</a>
+      <div className="menu-toggle" onClick={() => setOpen(!open)}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <nav className={open ? "nav active" : "nav"}>
         <a href="#sobre">Sobre</a>
+        <a href="#servicos">Serviços</a>
+        <a href="#contacto">Contacto</a>
         <a href="#contacto" className="btn-header">Marcar</a>
       </nav>
+
     </header>
   )
 }
+
+export default Header
